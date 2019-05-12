9c96f5ff3977078349b0b7102c201075c8521668
Luisfayre <luisfayre@hotmail.com>
2019-05-12 16:36:09 -0600

    historia v.2

--- historia.php
+++ historia.php
@@ -66,56 +66,9 @@
 
                 </div>
                 
-                <!-- Widgets -->
-                
-                <div class="col-md-4">
-                    
-                    <br>
-                    <br>
-                    <br>
-                    <br><br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    
-                 
-                          <div class="text-center">
-                        <p>
-                            <a href="php/depi.php" target="_blank">
-                                <img src="img/logos/depi.png" alt="depi "height="100 " width="195" style="max-width: 100%;">
-                            </a>
-                        </p>
-
-                        <p>
-                            <a href="http://sii.itchihuahuaii.edu.mx/ " target="_blank ">
-                                <img src="img/logos/SII.jpg " height="100 " width="195" style="max-width: 100%;">
-                            </a>
-                        </p>
-
-                         <!-- Calendario -->
-                         <p>
-                            <iframe id="minical" scrolling="no" frameborder="0" src='calendar.html' width="258px" height='275px'></iframe>
-                        </p>
-                        <!-- Fin calendario -->
-                        <p>
-                            <a href="http://www.conricyt.mx" target="_blank">
-                                <img src="img/logos/conricyt.jpg" border="0" height="105" width="232">
-                            </a>
-                        </p>
-                        <p>
-                            <a href="http://capacitacion.conricyt.mx/ " target="_blank ">
-                                <img src="img/logos/conricytcap.jpg " border="0 " height="105 " width="232 ">
-                            </a>
-                        </p>
-                        <p>
-                            <a href="http://www.cnbes.sep.gob.mx/">
-                                <img src="img/logos/cnbes.png" border="0" height="105" width="232">
-                            </a>
-                        </p>
-                    </div>
-
-                </div>
+                <?php
+                    require_once 'includes/widgets.php';
+                ?>
             </div>
         </div>
 
