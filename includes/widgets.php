86daf80b8bdf3dce799398e973cef7bae687979b
Luisfayre <luisfayre@hotmail.com>
2019-06-12 14:00:09 -0600

    widgets v1.4
    Se le agregan alt y title a todas las imagenes

--- includes/widgets.php
+++ includes/widgets.php
@@ -23,13 +23,13 @@
     <div class="text-center">
         <p>
             <a href="#">
-                <img src="img/logos/depi.png" alt="depi "height="100 " width="195" style="max-width: 100%;">
+                <img src="img/logos/depi.png" alt="depi" title="Depi" height="100 " width="195" style="max-width: 100%;">
             </a>
         </p>
 
         <p>
             <a href="http://sii.itchihuahuaii.edu.mx/ " target="_blank ">
-                <img src="img/logos/SII.jpg " height="100 " width="195" style="max-width: 100%;">
+                <img src="img/logos/SII.jpg " height="100 " width="195" style="max-width: 100%;" alt="Sistema Integral de Información" title="SII">
             </a>
         </p>
 
@@ -40,17 +40,17 @@
         <!-- Fin calendario -->
         <p>
             <a href="http://www.conricyt.mx" target="_blank">
-                <img src="img/logos/conricyt.jpg" border="0" height="105" width="232">
+                <img src="img/logos/conricyt.jpg" border="0" height="105" width="232" alt="conricyt" title="conricyt">
             </a>
         </p>
         <p>
             <a href="http://capacitacion.conricyt.mx/" target="_blank ">
-                <img src="img/logos/conricytcap.jpg " border="0 " height="105 " width="232 ">
+                <img src="img/logos/conricytcap.jpg " border="0 " height="105 " width="232" alt="conricytcap" title="conricytcap">
             </a>
         </p>
         <p>
             <a href="http://www.cnbes.sep.gob.mx/">
-                <img src="img/logos/cnbes.png" border="0" height="105" width="232">
+                <img src="img/logos/cnbes.png" border="0" height="105" width="232" alt="cnbes" title="cnbes">
             </a>
         </p>
     </div>
