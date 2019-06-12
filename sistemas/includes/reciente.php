2098e61352c490420a6851cfdbafecba61e007e6
Luisfayre <luisfayre@hotmail.com>
2019-06-12 15:24:05 -0600

    Includes sistemas
    Se añede includes a sistemas

---
+++ sistemas/includes/reciente.php
@@ -0,0 +1,109 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require_once './includes/mensaje_dia.php';
+?>
+<div class="divisor_sombra">
+</div> 
+<div id="4directorios" class="four_directories">
+    <div class="directorio-footer-column">
+        <div class="directorio-footer">
+            <div class="Directorio_title">RECIENTE</div>
+            <br>
+            <br>			
+            <div class="textwidget">
+                <!--Examen-->
+                <?php
+                if (verifica_rango($date_inicio, $date_fin2, $date_nueva)) {
+                    echo "
+                        <li class=\"listado\">
+                            <a href=\"http://moodle.itchihuahuaii.edu.mx/mod/quiz/view.php?id=10677\"target=\"_blank\">Examen
+                            </a>
+                        </li>";
+                }
+                ?>
+
+                <li class="listado">
+                    <a href="convocatoria_base.php">Maestría en Sistemas Computacionales
+                    </a>
+                </li>
+
+                <li class="listado">
+                    <a href="jornadaAcad4ta.php">Cuarta Jornada Académica
+                    </a>
+                </li>
+
+                <li class="listado">
+                    <a href="tercerjornadaacad.php">Tercer Jornada Académica
+                    </a>
+                </li>
+                <!--li class="listado">
+                    <a href="http://www.itchihuahuaii.edu.mx/?page_id=1726">Ambiental</a>
+                </li>
+                <li class="listado">
+                    <a href="http://www.itchihuahuaii.edu.mx/?page_id=880">Calidad</a>
+                </-->
+            </div>
+        </div></div><div class="directorio-footer-column">
+        <div class="directorio-footer">
+            <div class="Directorio_title">COLOQUIOS</div>
+            <br>
+            <br>
+            <div class="textwidget">
+                <li class="listado">
+                    <a href="coloquio_Dic-2017.php">Diciembre 2017</a>
+                </li>
+                <li class="listado">
+                    <a href="coloquio_Ene-Jun-2017.php">Enero 2017</a>
+                </li>
+                <li class="listado">
+                    <a href="coloquio_Ago-Dic-2016.php">Agosto 2016</a>
+                </li>
+                <li class="listado">
+                    <a href="m_coloqui_Ene-Jun_2016.php">Enero 2016</a>
+                </li>
+                <li class="listado">
+                    <a href="m_coloquio201.php">Dieciembre 2015</a>
+                </li>
+                <!--li class="listado">
+                    <a href="https://drive.google.com/file/d/17mBUQIqlmICVW7VMkoUrKRrbMMNFBCvG/view?usp=sharing">Constitución Política de los E.U.M.</a>
+                </li>
+                <li class="listado"><a href="http://www.itchihuahuaii.edu.mx/?page_id=1306">Leyes</a>
+                </li>
+                -->
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="directorio-footer-column">
+        <div class="directorio-footer">
+            <div class="Directorio_title">TEMAS DE INTERÉS</div><br><br>		
+            <div class="textwidget"><li class="listado"><a href="http://www.conricyt.mx/">CONRICyT</a></li>
+                <li class="listado">
+                    <a href="http://www.itchihuahuaii.edu.mx/">Movilidad</a></li>
+                <li class="listado">
+                    <a href="https://drive.google.com/file/d/12NPXsVBKm3Ewoi6RoikTR2cBJyQtRYjr/view?usp=sharing">Conclusión Administración 2012-2018</a>
+                </li>
+                <li class="listado">
+                    <a href="https://drive.google.com/file/d/1AWzBBwT-5ppauxqdlhwfTF0-y5SWuhpi/view?usp=sharing">Rendición de Cuentas 2018</a>
+                </li>
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="directorio-footer-column">
+        <div class="directorio-footer"><div class="Directorio_title">RELACIONADO</div>
+            <br>			<div class="textwidget"><li class="listado"><a href="http://www.itchihuahuaii.edu.mx/?page_id=867">Inglés</a></li>
+                <li class="listado"><a href="http://moodle.itchihuahuaii.edu.mx/">Moodle</a></li><li class="listado"><a href="https://www.tecnm.mx/convocatorias">Convocatorias</a></li>
+                <li class="listado"><a href="http://www.itchihuahuaii.edu.mx/?page_id=871">Multiverso TecNM</a></li>
+                <li class="listado"><a href="http://www.itchihuahuaii.edu.mx/?page_id=1404">Transparencia</a></li></div>
+        </div>
+    </div>
+</div>	
---
+++ sistemas/includes/widgets.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<!-- Widgets -->
+
+<div class="col-md-4">
+
+    <br>
+    <br>
+    <br>
+    <br><br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+
+    <div class="text-center">
+        <p>
+            <a href="#">
+                <img src="../../img/logos/depi.png" alt="depi" title="Depi" height="100 " width="195" style="max-width: 100%;">
+            </a>
+        </p>
+
+        <p>
+            <a href="http://sii.itchihuahuaii.edu.mx/ " target="_blank ">
+                <img src="../../img/logos/sii.png" height="100 " width="195" style="max-width: 100%;" alt="Sistema Integral de Información" title="SII">
+            </a>
+        </p>
+
+        <!-- Calendario -->
+        <p>
+            <iframe id="minical" scrolling="no" frameborder="0" src='calendar.html' width="258px" height='275px'></iframe>
+        </p>
+        <!-- Fin calendario -->
+        <p>
+            <a href="http://www.conricyt.mx" target="_blank">
+                <img src="../../img/logos/conricyt.jpg" border="0" height="105" width="232" alt="conricyt" title="conricyt">
+            </a>
+        </p>
+        <p>
+            <a href="http://capacitacion.conricyt.mx/" target="_blank ">
+                <img src="../../img/logos/conricytcap.jpg " border="0 " height="105 " width="232" alt="conricytcap" title="conricytcap">
+            </a>
+        </p>
+        <p>
+            <a href="http://www.cnbes.sep.gob.mx/">
+                <img src="../../img/logos/cnbes.png" border="0" height="105" width="232" alt="cnbes" title="cnbes">
+            </a>
+        </p>
+    </div>
+
+</div>
+
+<!--Fin widgets-->
