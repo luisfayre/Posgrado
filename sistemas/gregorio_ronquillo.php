42e8f49af4ff33e513fd2428141bba5d117b58ac
Luisfayre <luisfayre@hotmail.com>
2019-06-13 15:11:04 -0600

    gregorio_ronquillo
    se agrega gregorio_ronquillo

---
+++ sistemas/gregorio_ronquillo.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap-->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <!-- Estilos-->
+        <link rel="stylesheet" href="../css/style_tec.css">
+        <link rel="stylesheet" href="../css/index.css">
+        <link rel="stylesheet" href="../css/style.css">
+        <!-- Funete-->
+        <link rel="stylesheet" href="../css/font-awesome.css">
+        <!-- Scripts-->
+        <script src="../js/jquery.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
+        <script src="../js/javascript.js"></script>
+        <script type="text/javascript" src="../js/jquery.slidepanel.js"></script>
+        <link rel="stylesheet" type="text/css" href="../css/jquery.slidepanel.css">
+        <title>Gregorio Ronquillo Máynez</title>
+        <script src="../js/ddmenu.js" type="text/javascript"></script>
+    </head>
+
+    <body class="bodyIndex">
+        <a id="ddmenuHeader" href="headerDEPI.html"></a>
+        <!--Barra de navegacion-->
+        <a id="ddmenuLink" href="barraMSist.html"></a>
+        <div class="container" style="text-align:justify;">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2>DIVISIÓN DE ESTUDIOS DE
+                        POSGRADO E INVESTIGACIÓN
+                    </h2>
+                </div>
+            </div>
+            <div class="row bloque">
+                <h4 class="text-center">Docente </h4>
+                <div class="jumbotron cv">
+                    <div class="container">
+                        <div class="text-center">
+                            <img clas="img-responsive" src="../img/cv/ronquillo (copia).jpg" alt="Gregorio Ronquillo Máynez">
+                            <h2>Gregorio Ronquillo Máynez</h2>
+                            <p>CORREO: camachoalberto@hotmail.com
+                                <br> Instituto Tecnológico de Chihuahua II: (052) 614.4425000.124
+                            </p>
+                        </div>
+                        <p>ADSCRIPCIÓN ACTUAL
+                            <br> 02/1978 - 02/2015 </p>
+                        <blockquote>
+                            <p>TECNOLÓGICO NACIONAL DE MÉXICO / INSTITUTO TECNOLÓGICO DE CHIHUAHUA II, Profesor de tiempo completo</p>
+                        </blockquote>
+                        <p>ARTÍCULOS </p>
+                        <blockquote>
+                            <table>
+                                <tr>
+                                    <td valign=top>2014&nbsp;</td>
+                                    <td><strong>SOFTWARE ASÍNCRONO PARA LA GESTIÓN DEL CURSO EN EL SISTEMA DE GESTIÓN DE LA CALIDAD</strong>, 7MO. Congreso Internacional de Investigación CIPITECH 2014.</td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2014&nbsp;</td>
+                                    <td><strong>IMPORTANCIA DE LA PERCEPCION DE LOS SISTEMAS DE GESTION DE CALIDAD Y LAS TECNOLOGIAS DE LA INFORMACION</strong>, Revista de la Facultad de Contaduría y Administración de la Universidad Autónoma de Chihuahua.</td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2013&nbsp;</td>
+                                    <td><strong>ANÁLISIS TOTAL PARA LA CALIDAD TOTAL Y LA INGENIERÍA DE SOFTWARE</strong>, ISBN: 978-607-8262-02-1. CIPITECH.</td>
+                                </tr>
+                            </table>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Pie de pagina-->
+    <div class="container">
+        <?php
+        require_once './includes/footer.php';
+        ?>
+    </div>
+
+
+</body>
+
+</html>
