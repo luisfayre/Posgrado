8c9ebe572ca22aafe96f8a505ecf1f47a00c0ae9
Luisfayre <luisfayre@hotmail.com>
2019-06-18 15:09:26 -0600

    coordinadores includes

---
+++ includes/coordinadores.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * Este PHP sirve para que solo de modifique una seccion de coordinadores
+ * y para que se vean reflejadas en todas las paginas que tengan este include
+ * esto con el fin de reducir el trabajo de copiar y pegar el mismo codigo en
+ * todas las paginas
+ */
+?>
+<div class="media marcos thumbnail">
+    <div class="media-left media-middle">
+        <a href="#">
+            <img class="media-object" src="img/cv/Ilya_2.jpg" alt="Ilya" width=100 height=162>
+        </a>
+    </div>
+    <div class="media-body">
+        <h4 class="media-heading">Ilya Lorena Sánchez Rivera</h4>
+
+        <p>Correo: 
+            <a href="mailto:ilyasanchez@hotmail.com">ilyasanchez@hotmail.com</a>
+        </p>
+        <p> 16/09/1998 - 2019 Tecnológico Nacional De México / Instituto Tecnológico De Chihuahua II,
+            Coordinadora de la Maesría en Sistemas Computacionales, Tiempo completo</p>
+        <p>
+        </p>
+    </div>
+</div>
+
+
+<!--div class="media marcos thumbnail">
+    <div class="media-left media-middle">
+        <a href="#">
+            <img class="media-object" src="" alt="" width=100 height=162>
+        </a>
+    </div>
+    <div class="media-body">
+        <h4 class="media-heading"></h4>
+        <p>Correo: 
+            <a href="mailto:posgradomii@hotmail.com">posgradomii@hotmail.com</a>
+        </p>
+
+        <p>
+            Fecha
+        </p>
+    </div>
+</div-->
---
+++ sistemas/includes/coordinadores.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Este PHP sirve para que solo de modifique una seccion de coordinadores
+ * y para que se vean reflejadas en todas las paginas que tengan este include
+ * esto con el fin de reducir el trabajo de copiar y pegar el mismo codigo en
+ * todas las paginas
+ */
+?>
+<div class="row">
+    <div class="col-md-12 col-sm-12 text-center">
+        <h2>Coordinadores</h2>
+        <div class="col-sm-6 col-md-5 col-md-offset-1">
+            <h3>Maestría en Ingeniería Industrial</h3>
+            <div class="thumbnail">
+                <!--img src="" alt="..."-->
+                <div class="caption ">
+                    <h4></h4>
+                </div>
+                <div class="caption">
+                    <p>posgradomii@hotmail.com</p>
+                    <p>Teléfono:52 (614) 442-5032</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-5">
+            <h3>Maestría en Sistemas Computacionales</h3>
+            <div class="thumbnail">
+                <img src="../img/cv/Ilya.jpg" alt="...">
+                <div class="caption ">
+                    <h4>M.S.I Ilya Sánchez</h4>
+                </div>
+                <div class="caption">
+                    <p>ilyasanchez@hotmail.com
+                    <p>Teléfono:52 (614) 442-5032</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
