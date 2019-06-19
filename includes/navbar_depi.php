01538383d5eb1da115300e44881209ba96b17d8c
Luisfayre <luisfayre@hotmail.com>
2019-06-18 21:46:27 -0600

    barraDEPI a navbar_depi

---
+++ includes/navbar_depi.php
@@ -0,0 +1,177 @@
+
+<!--nav id="navbar" class="navbar" data-snavbarfix" data-offset-top="280" role="navigation" style="background: #eaeaea;">
+    <div class="container">
+
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#" style="color: #000000;">
+                DEPI
+            </a>
+        </div>
+        <div class="collapse navbar-collapse" id="subenlaces">
+            <ul class="nav navbar-nav navbar-right">
+
+                <li class="dropdown">
+                    <a href="index.php" target="_top">Inicio</a>
+                </li>
+
+                <li class="dropdown">
+                    <a href="_top" class="dropdown-toggle" data-toggle="dropdown">Información</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="historia.php">Historia</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="mision.php"">Misión y Visión</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="documentos/calendarios/calendario-ene-jun-2019.pdf"
+                               target='_blank'>Calendario</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="http://www.sistemasycomputacion.mx/medios/">Medios de Verificación</a></li>
+                    </ul>
+                </li>
+
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">POSGRADOS</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="m_sistemas.php"">Maestría en Sistemas Computacionales</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="m_industrial.php"">Maestría en Ingeniería Industrial</a>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
+
+
+    </div>
+
+</nav-->
+
+
+<div class="container">
+    <div class="navbar bs-docs-nav col-md-12" role="banner">
+
+        <div class="navbar-header">
+            <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#navbar">
+                <span class="sr-only">Toggle navigation</span> 
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand visible-xs" href="index.php">
+                <h1 class="principal" style="font-size: 1.5rem">DEPI</h1>
+            </a>
+
+        </div>
+
+        <nav id="navbar" class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+            <ul class="nav nav-justified text-center">
+                <li class="dropdown">
+                    <a href="index.php" target="_top">INICIO</a>
+                </li>
+                <li class="dropdown">
+                    <a href="_top" class="dropdown-toggle" data-toggle="dropdown">INFORMACIÓN</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="historia.php">Historia</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="mision_vision.php">Misión y Visión</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="documentos/calendarios/calendario-ene-jun-2019.pdf"
+                               target='_blank'>Calendario</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="http://www.sistemasycomputacion.mx/medios/">Medios de Verificación</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">POSGRADOS</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="sistemas/">Maestría en Sistemas Computacionales</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="industrial/">Maestría en Ingeniería Industrial</a>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">SERVICIOS</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="http://moodle.itchihuahuaii.edu.mx/">Moodle para
+                                posgrados</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="documentos.php">Documentos</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="http://sii.itchihuahuaii.edu.mx/">Sistema Integral De Información(SII)</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="http://www.itchihuahuaii.edu.mx">Tecnológico de Chihuahua II</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="encuesta.php">ENCUESTA</a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">ADMINISTRATIVO</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="convocatoria.php">Requisitos de ingreso</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="proceso_titulacion.php">Proceso de Titulación</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="normatividad.php">Normatividad</a></li>
+                    </ul>
+                </li>
+
+
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">NOTICIAS</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="convocatoria2017.php">Convocatoria 2018-1</a></li>
+                        <li role="separator" class="divider">
+
+                        </li>
+                        <li>
+                            <a href="coloquio_Ene-Jun-2017.php">Coloquio Enero-Junio 2017</a>
+                        </li>
+                        <li role="separator" class="divider">
+                        </li>
+                        <li>
+                            <a href="coloquio_Dic-2017.php">Coloquio Diciembre 2017</a>
+                        </li>
+                        <li role="separator" class="divider"></li>
+                        <li>
+                            <a href="coloquio_Ago-Dic-2016.php">Coloquio Ago-Dic 2016</a>
+                        </li>
+                        <li role="separator" class="divider"></li>
+                        <li>
+                            <a href="m_coloqui_Ene-Jun_2016.php">Coloquio Ene-Jun 2016</a>
+                        </li>
+                        <li role="separator" class="divider"></li>
+                        <li>
+                            <a href="tercerjornadaacad.php">Tercer Jornada Academica</a>
+                        </li>
+                        <li role="separator" class="divider"></li>
+                        <li>
+                            <a href="inducainv.php">Inducción a Investigación</a>
+                        </li>
+                        <li role="separator" class="divider"></li>
+                        <li>
+                            <a href="graduacion2g.php">Graduación 2<sup>a</sup> Gen.</a>
+                        </li>
+                        <li role="separator" class="divider"></li>
+                        <li>
+                            <a href="m_convocatoria16_En-Jun.php">Convocatoria Ene-Jun 2016</a>
+                        </li>
+                        <li role="separator" class="divider"></li>
+                        <li>
+                            <a href="m_coloquio201.php">Coloquio Ago-Dic 2015</a>
+                        </li>
+                        <li role="separator" class="divider"></li>
+                        <li>
+                            <a href="convocatoria-2015.php">Convocatoria Ago-Dic 2015</a>
+                        </li>
+                        <li role="separator" class="divider">
+
+                        </li>
+                        <li><a href="toastmasters.php">ToastMasters 2015</a>
+                        </li>
+                    </ul>
+                </li> 
+            </ul>
+        </nav>
+    </div>
+</div>
