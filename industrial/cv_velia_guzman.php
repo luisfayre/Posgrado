30463e204e351ff9b83b93f2ff1d6466ce655b05
Luisfayre <luisfayre@hotmail.com>
2019-06-19 20:32:36 -0600

    sin utilizar

---
+++ industrial/cv_velia_guzman.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/javascript.js"></script>
+    <script type="text/javascript" src="js/jquery.slidepanel.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/jquery.slidepanel.css">
+    <title>Rosa Maria Amaya</title>
+    <script src="js/ddmenu.js" type="text/javascript"></script>
+</head>
+
+<body class="bodyIndex">
+    <a id="ddmenuHeader" href="headerDEPI.html"></a>
+    <!--Barra de navegacion-->
+    <a id="ddmenuLink" href="barraDEPI.html"></a>
+    <div class="container" style="text-align:justify;">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2>DIVISIÓN DE ESTUDIOS DE
+                    POSGRADO E INVESTIGACIÓN
+                </h2>
+            </div>
+        </div>
+        <div class="row bloque">
+            <h4 class="text-center">Docente </h4>
+            <div class="jumbotron cv">
+                <div class="container">
+                    <div class="text-center">
+                        <img clas="img-responsive" src="img/cv/rosy.jpg" alt="">
+                        <h2>M.C. Rosa Ma. Amaya Toral</h2>
+                        <p>CORREO:
+                            <br> Instituto Tecnológico de Chihuahua II:
+                        </p>
+                    </div>
+                    <p>ADSCRIPCIÓN ACTUAL
+                        <br>
+                    </p>
+                    <blockquote>
+                        <p>-</p>
+                    </blockquote>
+                    <p>ARTÍCULOS </p>
+                    <blockquote>
+                        <table>
+                            <tr>
+                                <td valign=top>2014&nbsp;</td>
+                                <td><strong></td>
+                            </tr>
+                        </table>
+                    </blockquote>
+                    <p>NIVELES/GRADOS ACADÉMICOS </p>
+                    <blockquote>
+                        <table>
+                            <tr>
+                                <td valign=top>-&nbsp;</td>
+                                <td><strong>MAESTRIA</strong>,Ciencias en Ingeniería Industrial del Instituto
+                                    Tecnológico de Cd. Juárez, cédula: 2840407).</td>
+                            </tr>
+                            <tr>
+                                <td valign=top>-&nbsp;</td>
+                                <td><strong>LICENCIATURA</strong>Ingeniería Industrial en Producción. Instituto
+                                    Tecnológico de Chihuahua, cédula: 1541069.</td>
+                            </tr>
+                        </table>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!--Pie de pagina-->
+    <iframe scrolling="no" frameborder=0 src='footerDEPI.html' width='100%' height='350px'></iframe>
+</body>
+
+</html>
