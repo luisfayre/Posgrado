dffc55bcafe45ce8320db1a7ac86b4c32e1e60b3
Luisfayre <luisfayre@hotmail.com>
2019-08-25 23:24:53 -0600

    Informes v1.1.1.1
    Solo con modificar el include/informes toda la seccion informes sera modificada automaticamente

---
+++ includes/informes.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Con este PHP podremos modificar los informes mas rapidamesnte y en cada una de las paginas
+ */
+?>
+<p style = "text-align:center"><b>INFORMES:<br>M.I.S.C Jesús Arturo Alvarado Granadino</b>
+<br>Correo: posgrado@itchihuahuaii.edu.mx
+<br>
+<br>
+<b>M.S.I. Ilya Lorena Sánchez Rivera</b>
+<br>Correo: ilyasanchez@hotmail.com
+<br>
+<br>
+<br>Web: <a href = "http://www.sistemasycomputacion.mx/">www.sistemasycomputacion.mx</a>
+<br>Facebook: <a href = "https://www.facebook.com/itchii.posgrados">www.facebook.com/itchii.posgrados</a>
+<br>Twitter: <a href = "https://twitter.com/posgradositch2">@posgradositch2</a>
+<br>
+<br>
+<b>División de Estudios de Posgrado e Investigación.</b>
+<br>Coordinación de Posgrado Sistemas Computacionales.
+<br>
+<br>Av. De las Industrias #11101, Complejo Industrial Chihuahua. Tel: 442-50-00 ext. 132.</p>
