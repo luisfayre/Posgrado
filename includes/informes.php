7189ccfc7b584861a49bed34c6473634e615c35b
JenrruPro <50652828+JenrruPro@users.noreply.github.com>
2019-12-19 11:57:18 -0700

    Update informes.php

--- includes/informes.php
+++ includes/informes.php
@@ -7,12 +7,12 @@
 <p style = "text-align:center"><b>INFORMES:<br>Maestría en Ingeniería Industrial<br>M.S.M Nidia Yasmina Rico Ramos</b>
 <br>Correo: posgradoindustrialitchii@gmail.com
 <br>
-<br>Web: <a href = "http://www.sistemasycomputacion.mx/">www.sistemasycomputacion.mx</a>
+<br>Web: <a href = "http://www.itchihuahuaii.edu.mx/webmaestria/industrial/">www.itchihuahuaii.edu.mx/webmaestria/industrial/</a>
 <br>Facebook: <a href = "https://www.facebook.com/itchii.posgrados">www.facebook.com/itchii.posgrados</a>
 <br>Twitter: <a href = "https://twitter.com/posgradositch2">@posgradositch2</a>
 <br>
 <br>
 <b>División de Estudios de Posgrado e Investigación.</b>
-<br>Coordinación de Posgrado Sistemas Computacionales.
+<br>Coordinación de Posgrado de Ingeniería Industrial.
 <br>
 <br>Av. De las Industrias #11101, Complejo Industrial Chihuahua. Tel: 442-50-00 ext. 132.</p>
