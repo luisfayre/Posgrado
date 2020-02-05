14ccd019a32028fd965c362d924eb1b877205f12
JenrruPro <50652828+JenrruPro@users.noreply.github.com>
2020-02-04 18:50:10 -0700

    Create convocatoriaISC.php

---
+++ convocatoriaISC.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="es">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap-->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Estilos-->
+        <link rel="stylesheet" href="css/style_tec.css">
+        <link rel="stylesheet" href="css/index.css">
+        <!-- Funete-->
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <!-- Scripts-->
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/javascript.js"></script>
+        <script type="text/javascript" src="js/jquery.slidepanel.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/jquery.slidepanel.css">
+
+
+        <title>Convocatoria 2020-1</title>
+    </head>
+
+    <body class="bodyIndex">
+        <?php
+        require_once 'includes/header_depi.php';
+        ?>
+        <!--Navbar-->
+        <div class="container">
+            <?php
+            require_once 'includes/navbar_depi.php';
+            ?>
+        </div>
+
+
+        <!-- Informacion Convocatoria -->
+        <div class="container">
+            <div class="col-md-12">
+                <div class="col-md-8">
+
+                    <div class="row">
+
+                        <div class="col-md-12 text-center">
+                            <h2 class="fuente_titulos">CONVOCATORIA PARA ASPIRANTES<br>2020 - 1</h2>
+                        </div>
+
+                        <div>
+                            <h1 class="principal" style="text-align:center">MAESTRÍAS EN SISTEMAS COMPUTACIONALES</h1>
+                            <br>
+                            <br>
+                            <p>
+                                El Tecnológico de Chihuahua II a través de la División de Estudios de Posgrado e Investigación (DEPI), 
+                                hace una atenta invitación a que cursen nuestros programas de maestría. La convocatoria está abierta 
+                                y se recibirán solicitudes hasta el día 3 de enero de 2020 para el semestre enero-junio 2020.
+                            </p>
+
+                            <p>
+                                1. Para iniciar el trámite lo único que se les solicita es:
+                            </p>
+                            <div style="padding-left: 15px">
+                                <li>
+                                    Llenar solicitud, esta la encuentran en la página de Posgrado del ITCHI: 
+                                    <a class="btn bg-info"href="documentos/FORMATO-INGRESO-MAESTRIA-FSI-2019.docx">Solicitud</a></li>
+                                <li>
+                                    Dos cartas de recomendación de sus maestros de Licenciatura.
+                                </li>
+                                <li>
+                                    Carta de exposición de motivos.
+                                </li>
+                                <li>
+                                    Currículum Vitae Único con folio de CONACYT:
+                                    <a class="btn bg-info" href="https://miic.conacyt.mx/generador-view-angular/index.html?application=CVU#/login">Curriculum</a>
+                                </li>
+                                <li>
+                                    Currículum Vitae Profesional.
+                                </li>
+                            </div>
+
+                            <br>
+                            <p>
+                                Esta papelería deberá traerla a la DEPI del Instituto o bien enviarla por correo electrónico debidamente
+                                firmada, a la siguiente dirección electrónica:
+                                <a href="mailto:posgradoindustrialitchii@gmail.com">posgradoindustrialitchii@gmail.com</a>
+                            </p>
+                            <p>
+                                2. Presentar examen CENEVAL.
+                                <a class="btn bg-info" href="http://www.ceneval.edu.mx/exani-iii">
+                                    (registrarse en línea)
+                                </a>
+                                <div class="bg-danger" href="http://www.ceneval.edu.mx/exani-iii">
+                                    Recuerda consultar el (Calendario de aplicaciones)
+                                </div>
+                            </p>
+                            <p>
+                                3. Presentar examen de ingreso (10 de enero 2020).
+                                <a class="btn bg-info" href="documentos/GuiaExaConocmaestriaSC2.pdf">
+                                    (Guía)
+                                </a>
+                            </p>
+                            <div style="padding-left: 15px">
+                                <li>Costo $800.00.</li>
+                                <li>Pagar en caja de recursos financieros del mismo Instituto.</li>
+                            </div>
+                            <br>
+                            <p>
+                                4. Presentar examen de inglés (13 de enero 2020).
+                            </p>
+                            <div style="padding-left: 15px">
+                                <li>Costo $250.00.</li>
+                                <li>Pagar en caja de recursos financieros del mismo Instituto.</li>
+                            </div>
+                            <br>
+                            <p>
+                                5. Presentarse a una entrevista, la cual se programará con anticipación (17 de enero 2020).
+                            </p>
+                            <br>
+                            <p>
+                                6. La publicación de los resultados se dará a conocer el día 21 de enero 2020.
+                            </p>
+                            <br>
+                            <p>
+                                7. Una vez aceptado deberá hacer una propuesta de tema de tesis que dé solución a un problema real, ya que nuestra maestría es profesionalizante.
+                            </p>
+                            <br>
+                            <!--h3>NOTA:</h3>
+                            <p>Para concluir el trámite de postulascion a la masestrías, es necesario cumplir con todos los requisitos
+                                mencionados en los puntos del 1 al 5.</p>
+
+
+                            <table class="table">
+                                <thead>
+                                    <tr>
+
+                                        <th>Original para cotejar y copia</th>
+                                        <th>Documentos solicitados</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>COPIA DE COMPROBANTE DE CUOTA POR CONCEPTO DE INSCRIPCIÓN</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>CARTA DE ACEPETACIÓN AL PROGRAMA DE MAESTRÍA</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>TITULO Y CÉLUDA DE PROESIONAL O ACTA DE ÉXAMEN PROFESIONAL</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>COPIA DEL CERTIFICADO DE ESTDIOS DE LICENCIATURA</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>CARTA DE PASANTE Y CARTA DE COMPROMISO PARA TITULACAIÓN (Titulación por estudios de
+                                            maestría
+                                            exclusivamente).
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>1</td>
+                                        <td>IMPRESIÓN DE CURP
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>1</td>
+                                        <td>COPIA DE CREDENCIAL DE ELECTOR POR AMBOS LADOS
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>1</td>
+                                        <td>ACTA DE NACIMIENTO</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>1</td>
+                                        <td>DOS FOTOGRAFIAS RECIENTES, TAMALO CREDENCIAL</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>1</td>
+                                        <td>
+                                            COPIA FORMATO FM9 (En caso de ser extranjero)</td>
+                                    </tr>
+
+                                </tbody>
+                            </table-->
+
+
+
+                            <!-- Costos -->
+                            <h3>Costos</h3>
+                            <div style="padding-left: 15px">
+                                <li>Inscripción:    $1400.00</li>
+                                <li>Costo por materia $1700.00</li>
+                                <li>Seminarios $1000.00 </li>
+                                <li>Tesis $5200.00.</li>
+                            </div>
+                            <br>
+                            <h3>Beca CONACYT</h3>
+                            <p>
+                                Esta beca aplica únicamente para la Maestría en Sistemas Computacionales.
+                                Para esta se requiere ser aceptado y estar inscrito en el programa de maestría como alumno de tiempo
+                                completo y de dedicación completa al programa, es decir, no estar trabajando en ninguna empresa.
+                                <!--a
+                                    href="https://www.conacyt.gob.mx/index.php/convocatorias-b-nacionales/convocatorias-abiertas-becas-nacionales/16819-conv-bn-18/file">
+                                    Más Información</a-->
+                                <br>
+                            <h3>Requisitos</h3>
+                            <div style="padding-left: 15px">
+                                <li>
+                                    Promedio de 85.
+                                </li>
+                                <li>
+                                    Dominar el idioma inglés (contar con 540 puntos de TOFEL).
+                                </li> 
+                            </div>
+
+                            <br><br><br>
+                        </div>
+                    </div>
+                </div>
+
+                <?php
+                require_once 'includes/widgets.php';
+                ?>
+            </div>
+        </div>
+
+
+
+
+        <!-- Informes -->
+        <!--a id="ddmenuLink3" href="informes.html"></a-->
+
+
+
+
+        <div class="container">
+           <?php
+            require_once 'includes/informesISC.php';
+            ?>
+
+        </div>
+
+
+
+        <div class="container">
+            <?php
+            require_once 'includes/reciente.php';
+            ?>
+
+            <?php
+            require_once 'includes/footer.php';
+            ?>
+        </div>
+
+
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141677500-1"></script>
+    </body>
+
+</html>
