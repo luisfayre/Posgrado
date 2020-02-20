f14504f594c2fdd6894aed96c127c1540a628602
JenrruPro <50652828+JenrruPro@users.noreply.github.com>
2020-02-19 20:18:23 -0700

    Update informesISC.php

--- includes/informesISC.php
+++ includes/informesISC.php
@@ -4,12 +4,10 @@
  * Con este PHP podremos modificar los informes mas rapidamesnte y en cada una de las paginas
  */
 ?>
-<p style = "text-align:center"><b>INFORMES:<br>M.I.S.C Jesús Arturo Alvarado Granadino</b>
-<br>Correo: posgrado@itchihuahuaii.edu.mx
-<br>
+<p style = "text-align:center"><b>INFORMES:
 <br>
 <b>M.S.I. Ilya Lorena Sánchez Rivera</b>
-<br>Correo: ilyasanchez@hotmail.com
+<br>Correo: ilya.sr@chihuahua2.tecnm.mx
 <br>
 <br>Web: <a href = "http://www.sistemasycomputacion.mx/">www.sistemasycomputacion.mx</a>
 <br>Facebook: <a href = "https://www.facebook.com/itchii.posgrados">www.facebook.com/itchii.posgrados</a>
