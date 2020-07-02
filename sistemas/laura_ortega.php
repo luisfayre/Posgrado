ceb26c1d38e4e27905939d84fb66a4df3bdcf9a8
SlickGeek <50652828+SlickGeek@users.noreply.github.com>
2020-07-02 13:53:38 -0600

    Create laura_ortega.php

---
+++ sistemas/laura_ortega.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap-->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <!-- Estilos-->
+        <link rel="stylesheet" href="../css/style_tec.css">
+        <link rel="stylesheet" href="../css/index.css">
+        <link rel="stylesheet" href="../css/style.css">
+        <!-- Funete-->
+        <link rel="stylesheet" href="../css/font-awesome.css">
+        <!-- Scripts-->
+        <script src="../js/jquery.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
+        <script src="../js/javascript.js"></script>
+        <script type="text/javascript" src="../js/jquery.slidepanel.js"></script>
+        <link rel="stylesheet" type="text/css" href="../css/jquery.slidepanel.css">
+        <title>Rafael Sandoval Rodriguez</title>
+        <script src="../js/ddmenu.js" type="text/javascript"></script>
+    </head>
+
+    <body class="bodyIndex">
+        <a id="ddmenuHeader" href="headerDEPI.html"></a>
+        <!--Barra de navegacion-->
+        <a id="ddmenuLink" href="barraMSist.html"></a>
+        <div class="container" style="text-align:justify;">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2>DIVISIÓN DE ESTUDIOS DE
+                        POSGRADO E INVESTIGACIÓN
+                    </h2>
+                </div>
+            </div>
+            <div class="row bloque">
+                <h4 class="text-center">Docente </h4>
+                <div class="jumbotron cv">
+                    <div class="container">
+                        <div class="text-center">
+                            <img clas="img-responsive" src="../img/cv/laura_ortega.jpg" alt="Laura Antonia Ortega Chávez">
+                            <h2>Dra. Laura Antonia Ortega Chávez</h2>
+                            <p>CORREO: laura.ortegach@gmail.com
+                                <br> Instituto Tecnológico de Chihuahua II
+                            </p>
+                        </div>
+                        <p>ADSCRIPCIÓN ACTUAL
+                            <br> 02/1978 - 02/2015 </p>
+                        <blockquote>
+                            <p>Instituto tecnológico de los Mochis / Instituto Tecnológico de Chihuahua II, profesor de tiempo completo</p>
+                        </blockquote>
+                        <p>Capítulos de libros</p>
+                        <blockquote>
+                            <table>
+                               <tr>
+                                    <td valign=top>2010&nbsp;</td>
+                                    <td><strong>Scene Recognition Based on Fusion of Color and Corner Features, Soft Computing for Recognition Based on Biometrics</strong>, M. I. Chacon M., C. P. Guerrero S., R. Sandoval R., SCI 312, pp. 317-331, Springer-Verlag , 2010.</td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2010&nbsp;</td>
+                                    <td><strong>Networked Control Systems: Algorithms and Experiments”, on Applications of Time Delay Systems, Lecture Notes in Control and Information Sciences</strong>, Rafael Sandoval-Rodriguez, Chaouki T. Abdallah, Henry N. Jerez, Ivan Lopez- Hurtado, Oscar Martinez-Palafox, and Dongjun Lee,  Springer-Verlag Edited volume 352,, John Chiasson and Jean Jacques Loiseau, Editors 2007, pp 37-56. ISBN 978-3-540- 49555-0.</td>
+                                </tr>
+
+                            </table>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Pie de pagina-->
+    <div class="container">
+        <?php
+        require_once './includes/footer.php';
+        ?>
+    </div>
+
+
+</body>
+
+</html>
