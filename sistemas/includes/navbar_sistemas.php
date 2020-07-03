ca5bb143d46b4b49f89b7f3a78972d8aa10e77af
JenrruPro <50652828+JenrruPro@users.noreply.github.com>
2020-07-02 21:04:08 -0600

    Update navbar_sistemas.php

--- sistemas/includes/navbar_sistemas.php
+++ sistemas/includes/navbar_sistemas.php
@@ -1,4 +1,12 @@
-
+<style>
+    #ppp{
+        color: rgb(255, 255, 255);
+    }
+    #ppp:hover{
+        color: rgb(27, 57, 106);
+    }
+    
+</style>
 
 <div class="navbar bs-docs-nav col-md-12" role="banner">
     <div class="navbar-header">
@@ -7,50 +15,50 @@
         </button>
     </div>
     <nav id="navbar" role="navigation">
-        <ul class="nav nav-justified text-center">
+        <ul class="nav nav-justified text-center" style="background-color: rgb(27, 57, 106);">
             <li>
-                <a href="../">INICIO</a>
+                <a href="../"><p id="ppp">INICIO</p></a>
             </li>
             <li class="dropdown">
-                <a href="perfil.php" target="_top">PERFIL</a>
+                <a href="perfil.php" target="_top"><p id="ppp">PERFIL</p></a>
             </li>
             <li class="dropdown">
-                <a href="objetivos.php" target="_top">OBJETIVO</a>
+                <a href="objetivos.php" target="_top"><p id="ppp">OBJETIVO</p></a>
             </li>
             <li class="dropdown">
-                <a href="plan.php" target="_top">PLAN DE ESTUDIOS</a>
+                <a href="plan.php" target="_top"><p id="ppp">PLAN DE ESTUDIOS</p></a>
             </li>
             <li class="dropdown">
-                <a href="cohorte.php" target="_top">ALUMNOS COHORTE</a>
+                <a href="cohorte.php" target="_top"><p id="ppp">ALUMNOS COHORTE</p></a>
             </li>
             <li class="dropdown">
-                <a href="nucleo.php" target="_top">NÚCLEO ACADÉMICO</a>
+                <a href="nucleo.php" target="_top"><p id="ppp">NÚCLEO ACADÉMICO</p></a>
             </li>
         </ul>
     </nav>
     <nav id="navbar" role="navigation">
-        <ul class="nav  nav-justified text-center">
+        <ul class="nav  nav-justified text-center"  style="background-color: rgb(27, 57, 106);">
             <li class="dropdown">
-                <a href="lineas.php" target="_top">LGAC</a>
+                <a href="lineas.php" target="_top"><p id="ppp">LGAC</p></a>
             </li>
             <li class="dropdown">
-                <a href="tutoriaAct.php" target="_top">TUTORIA</a>
+                <a href="tutoriaAct.php" target="_top"><p id="ppp">TUTORIA</p></a>
             </li>
             <li class="dropdown">
-                <a href="productividad.php" target="_top">PUBLICACIONES</a>
+                <a href="productividad.php" target="_top"><p id="ppp">PUBLICACIONES</p></a>
             </li>
             <li class="dropdown">
-                <a href="_top" class="dropdown-toggle" data-toggle="dropdown">VINCULACIÓN</a>
+                <a href="_top" class="dropdown-toggle" data-toggle="dropdown"><p id="ppp">VINCULACIÓN</p></a>
                 <ul class="dropdown-menu">
                     <li><a href="colaboracion-instituciones.php">Colaboración con otras instituciones</a></li>
                     <li><a href="colaboracion-sectores.php">Colaboración con otros sectores de la sociedad</a></li>
                 </ul>
             </li>
             <li class="dropdown">
-                <a href="procesos.php" target="_top">PROCESOS</a>
+                <a href="procesos.php" target="_top"><p id="ppp">PROCESOS</p></a>
             </li>
             <li class="dropdown">
-                <a href="claustro.php" target="_top">CLAUSTRO</a>
+                <a href="claustro.php" target="_top"><p id="ppp">CLAUSTRO</p></a>
             </li>
         </ul>
     </nav>
