41a45baf35ee8a6dd2193d640630e68cbcf43076
JenrruPro <50652828+JenrruPro@users.noreply.github.com>
2020-07-02 21:03:32 -0600

    Update convocatoria.php

--- convocatoria.php
+++ convocatoria.php
@@ -46,7 +46,7 @@
                         </div>
 
                         <div>
-                            <h1 class="principal" style="text-align:center">MAESTRÍAS EN INGENIERÍA INDUSTRIAL</h1>
+                            <h1 class="principal" style="text-align:center; color:rgb(27, 57, 105);">MAESTRÍAS EN INGENIERÍA INDUSTRIAL</h1>
                             <br>
                             <br>
                             <p>
