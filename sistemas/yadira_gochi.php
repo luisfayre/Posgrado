510ccc317ecc9053130ac0b1e064a9bcaa4c726a
SlickGeek <50652828+SlickGeek@users.noreply.github.com>
2020-07-08 14:04:32 -0600

    Create yadira_gochi.php

---
+++ sistemas/yadira_gochi.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap-->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <!-- Estilos-->
+        <link rel="stylesheet" href="../css/style_tec.css">
+        <link rel="stylesheet" href="../css/index.css">
+        <link rel="stylesheet" href="../css/style.css">
+        <!-- Funete-->
+        <link rel="stylesheet" href="../css/font-awesome.css">
+        <!-- Scripts-->
+        <script src="../js/jquery.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
+        <script src="../js/javascript.js"></script>
+        <script type="text/javascript" src="../js/jquery.slidepanel.js"></script>
+        <link rel="stylesheet" type="text/css" href="../css/jquery.slidepanel.css">
+
+        <title>Yadira Gochi</title>
+        <script src="../js/ddmenu.js" type="text/javascript"></script>
+    </head>
+
+    <body class="bodyIndex">
+        <a id="ddmenuHeader" href="headerDEPI.html"></a>
+        <!--Barra de navegacion-->
+        <a id="ddmenuLink" href="barraMSist.html"></a>
+        <div class="container" style="text-align:justify;">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2>DIVISIÓN DE ESTUDIOS DE
+                        POSGRADO E INVESTIGACIÓN
+                    </h2>
+                </div>
+            </div>
+            <div class="row bloque">
+                <h4 class="text-center">Docente </h4>
+                <div class="jumbotron cv">
+                    <div class="container">
+                        <div class="text-center">
+                            <img clas="img-responsive" src="../img/cv/DraGochi.jpg" alt="">
+                            <h2>Dra. Yadira Gochi Ponce</h2>
+                            <p>CORREO: yadira.gp@chihuahua2.tecnm.mx
+                                <br> Instituto Tecnológico de Chihuahua II: (052) 614.4425000.124
+                            </p>
+                        </div>
+                        <p>ADSCRIPCIÓN ACTUAL
+                            <br> 15/12/2013 - 02/2020 </p>
+                        <blockquote>
+                            <p>TECNOLÓGICO NACIONAL DE MÉXICO / INSTITUTO TECNOLÓGICO DE CHIHUAHUA II, Profesor de tiempo completo</p>
+                        </blockquote>
+                        <p>ARTÍCULOS INDIZADOS</p>
+                        <blockquote>
+                            <table>
+                                <tr>
+                                    <td valign=top>2015&nbsp;</td>
+                                    <td><strong>ELECTROCATALIZADORES NANOESTRUCTURADOS PARA CELDAS DE COMBUSTIBLE</strong>, Yadira Gochi Ponce, Gabriel Alonso Núñez, Temas de Ciencia y Tecnología 19 número 56. 54 – 60. 
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2015&nbsp;</td>
+                                    <td><strong>SÍNTESIS Y CARACTERIZACIÓN DE PELÍCULAS ZrO2:Sm3+, </strong>preparadas por la técnica de rocío pirolítico ultrasónico Rafael Martínez Martínez, Efraín Zaleta Alejandre, Guillermo Juárez López, Evaristo I. Velázquez Cruz, Emori Alain Sérvulo Carballo, Yadira Gochi Ponce, Ciro Falcony Guajardo., Temas de Ciencia y Tecnología 19 número 56. 3-8.</td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2015&nbsp;</td>
+                                    <td><strong>SONOCHEMICAL SYNTHESIS AND CHARACTERIZATION OF PT/CNT, PT/TIO2, AND PT/CNT/TIO2 ELECTROCATALYSTS FOR METHANOL ELECTRO-OXIDATION, </strong>Z.I. Bedolla-Valdez, Y. Verde- Gómez, A.M. Valenzuela-Muñiz, Y. Gochi-Ponce, M.T. Oropeza-Guzmán, Gilles Berhault, G. Alonso-Núñez, Electrochimica Acta 186. 76–84. </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2017&nbsp;</td>
+                                    <td><strong>A GREEN SYNTHESIS OF COPPER NANOPARTICLES USING NATIVE CYCLODEXTRINS AS STABILIZING AGENTS</strong>, Javier Suárez- Cerda, Heriberto Espinoza-Gómez, Gabriel Alonso-Núñez, Ignacio A. Rivero, Yadira Gochi-Ponce, Lucía Z. Flores- López, Journal of Saudi Chemical Society. 21, 341– 348. 
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2017&nbsp;</td>
+                                    <td><strong>ULTRASONIC-ASSISTED GALVANIC DISPLACEMENT SYNTHESIS OF PT-PD/MWCNT FOR ENHANCED OXYGEN REDUCTION REACTION: EFFECT OF PT CONCENTRATION, </strong>J. R. Zapata-Fernández, Y. Gochi-Ponce, M. I. Salazar-Gastélum, E. A. Reynoso-Soto, F. Paraguay-Delgado, S. W. Lin, R. M. Félix-Navarro, Int. J. Hydrogen. Energy, 42(15) 9806- 9815. </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2017&nbsp;</td>
+                                    <td><strong>REMOCIÓN DE PLOMO EN AGUA A PARTIR DE MATERIAL NANOESTRUCTURADO, NANOTUBOS DE CARBONO SOPORTADOS EN ZEOLITA NATURAL, </strong>Holanda I. Cruz, Mayela S. Javier, Dulce I. Luis, María de J. Gil, Alma D. Pérez y Y. Gochi-Ponce, Avances en Ciencias e Ingeniería, ISSN: 0718-8706 Vol. 8, 2, 21-27. </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2018&nbsp;</td>
+                                    <td><strong>CHALCOGENIDES AND CARBON NANOSTRUCTURES: GREAT APPLICATIONS FOR PEM FUEL CELLS, </strong>Yadira Gochi-Ponce, Gabriel Alonso-Núñez, Nicolás Alonso-Vante and Mercedes Teresita Oropeza-Guzmán, Proton Exchange Membrane Fuel Cell, ISBN: 978-1-78923-067-3, Chap. 6, 99- 119. 
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2020&nbsp;</td>
+                                    <td><strong>PREPARACIÓN DE MATERIALES FUNCIONALMENTE GRADUADOS POR DEPOSICIÓN ELECTROFORÉTICA, </strong>López Robledo, C.S.; Gochi Ponce, Y.; Oropeza Guzmán, M.T. Revista de Ciencias Tecnológicas, ISSN 25941925. Vol. 3. 1-9.</td>
+                                </tr>
+                            </table>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Pie de pagina-->
+    <div class="container">
+        <?php
+        require_once './includes/footer.php';
+        ?>
+    </div>
+</body>
+
+</html>
