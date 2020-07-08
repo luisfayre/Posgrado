393c297f9d313ba8b5cc9d8cc7421e49734c9497
SlickGeek <50652828+SlickGeek@users.noreply.github.com>
2020-07-08 14:04:04 -0600

    Update index.php

--- sistemas/index.php
+++ sistemas/index.php
@@ -63,7 +63,6 @@
                                 actualizarte? ven con nosotros e inscríbete y cumple tus sueños de calidad, validado por el CONACYT.
                             </p>
 
-
                             <h2>Información del programa</h2>
 
                             <p>Coordinador: Ilya Lorena Sánchez Rivera</p>
@@ -71,157 +70,9 @@
                             <p>Teléfonos: 4-42-50-32</p>
                             <p>Horario de atención: 8 a.m. a 8 p.m.</p>
 
-                            <h2>Requisitos para ingresar</h2>
-
-                            <p>El Instituto Tecnológico de Chihuahua II, agradece tu confianza en nuestros Planes de Posgrado.
-                                Esperamos que una vez que seas seleccionado, la formación que recibas logre darte una mejor
-                                preparación académica y potenciar tus habilidades personales.
-                            </p> 
-
-                            <p>Estás por iniciar el proceso de admisión, te deseamos el mejor de los éxitos. </p> 
-
-                            <div class="bloque"></div>
-
-                            <h3>Los aspirantes a ingresar al programa de posgrado deberán:</h3>
-                            <ul>
-                                <li>Tener título de Licenciatura en un área afín a la del programa, según los antecedentes del plan de estudios.</li>
-                                <li>Promedio mínimo de ochenta (80) o equivalente (80/100) en estudios de Licenciatura.</li>
-                                <li>Cumplir con el perfil de ingreso.</li>
-                            </ul>
+                            
+                            
                         </div>
-
-                        <br>
-
-                        <div style="text-align: center;">
-                            <a href="../documentos/FORMATO-INGRESO-MAESTRIA-FSI-2019.docx" class="btn btn-danger">Solicitud para maestría</a>
-                            <a href="#admision" class="btn btn-warning">Proceso de admisión</a>
-                            <a href="#inscripcion" class="btn btn-warning">Proceso de Inscripción</a>
-                        </div>
-
-
-                        <!--div class="collapse" id="admision"--> 
-                        <br>
-                        <h2 id="admision">Proceso de admisión</h2>
-                        <p>
-                            <span style="font-style: italic;">Ver fechas actualizadas del Proceso de Admisión en la
-                                CONVOCATORIA, al final de esta página (la convocatoria sólo se muestra cuando esté
-                                vigente para el siguiente semestre).</span></p>
-                        <p>Se integrará un Comité de Admisión con al menos tres profesores del Consejo de Posgrado,
-                            quienes
-                            analizarán y valorarán la suficiencia académica y la capacidad para realizar
-                            investigación
-                            del aspirante, mediante los siguientes pasos:</p>
-                        <ol>
-                            <li>Asistir al curso taller “Preparación de la propuesta del tema de tesis”.</li>
-                            <br>
-                            <p><span style="font-style: italic;">Los pasos siguientes se pueden aclarar en diagrama
-                                    de flujo al final, en caso de dudas del aspirante acerca del Proceso de
-                                    Admisión</span></p>
-                            <li>Llenar la solicitud de ingreso (FSI-01). En este formato se solicitan datos
-                                generales del
-                                alumno aspirante, escuela de procedencia y documentación entregada. </li>
-                            <li>Entrega la Carta de Exposición de Motivos. Es un documento impreso donde se exponen
-                                los motivos
-                                por los cuales desea ingresar a la maestría (una cuartilla mínimo). Este documento
-                                debe
-                                ser firmado por el aspirante.</li>
-                            <li>Presentar dos cartas de recomendación de maestros de licenciatura (excluir maestros
-                                del Consejo
-                                de Posgrado del Tecnológico de Chihuahua II).</li>
-                            <li>Presentarse a la entrevista con el Comité de Admisión, según la fecha establecida
-                                por el
-                                coordinador de la maestría en conjunto con los miembros del Comité de Admisión.</li>
-                            <li>Presentarse a hacer los exámenes en las fechas definidas para ese periodo escolar
-                                (examen
-                                de Inglés y de conocimientos en el área a estudiar).</li>
-                            <li>En caso de ser aceptado por haber cumplido estrictamente con los puntos anteriores,
-                                se entrega
-                                la carta de aceptación al aspirante.</li>
-                        </ol>
-
-                        <h3>Costos:</h3>
-                        <ul>
-                            <li>Examen de Inglés: $250.00 M.N.</li>
-                            <li>Inscripción: $1400.00 M.N.</li>
-                            <li>Costo por materia: $1700.00 M.N. (excepto la de Seminario I, cuyo costo es $1000.00
-                                M.N.
-                                y la de Tesis, que cuesta $5200.00 M.N.)</li>
-                            <br> Se cursan tres materias por semestre.
-                        </ul>
-                        <br>
-
-                        <div class="text-center" style="text-align: center"> 
-                            <img id=logoresp src="../img/proceso_admision.png"
-                                 class="img-responsive" alt="Proceso de admisión maestría en sistemas"> 
-                        </div>
-
-
-                        <br>
-                        <!--/div-->
-
-                        <!--div class="collapse" id="inscripcion"-->
-
-                        <h2 id="inscripcion">Proceso de Inscripción</h2>
-
-                        <h3>Sólo para primer semestre</h3>
-
-                        <p>Después de cumplir con los pasos del proceso de admisión se procede a la inscripción en
-                            el primer semestre:
-                        </p>
-                        <ol>
-                            <li>El aspirante se presenta con coordinador de la maestría con los siguientes
-                                documentos para
-                                su revisión:
-                            </li>
-                            <ul>
-                                <li class="listado">Dos fotografías tamaño infantil.</li>
-                                <li class="listado">Registro impreso de que ya se dio Usted de alta en el CVU (Curriculum Vitae
-                                    Único) en la página de
-                                    <a href="http://siicyt.main.conacyt.mx:9096/psp/REGCYT3/?cmd=login&languageCd=ESP">
-                                        CONACYT
-                                    </a>.
-                                </li>
-                                <li class="listado">Curriculum vitae impreso.</li>
-                                <li class="listado">Copia de credencial de elector.</li>
-                                <li class="listado">Impresión de 
-                                    <a href="https://consultas.curp.gob.mx/CurpSP/gobmx/inicio.jsp"> 
-                                        CURP
-                                    </a>.
-                                </li>
-                                <li class="listado">Acta de nacimiento.</li>
-                                <li class="listado">Copia del formato FM9 (sólo en caso de ser extranjero).</li>
-                                <li class="listado">Certificado médico emitido por una Institución del Sector Salud.</li>
-                                <li class="listado">Copia de título, cédula o acta de examen profesional (validado por la Dirección
-                                    General
-                                    de Profesiones).
-                                </li>
-                                <li class="listado">Copia del Certificado de Estudios de la Licenciatura.</li>
-                            </ul>
-                            <li>El alumno lleva a Servicios Escolares los documentos correspondientes a ese
-                                departamento
-                                (Dos fotografías, certificado médico, copia de estudios de licenciatura y copia del
-                                título,
-                                cédula o acta de examen profesional).</li>
-                            <li>El departamento de Servicios Escolares entrega constancia donde se notifica que el
-                                alumno
-                                entregó los documentos.</li>
-                            <li>El coordinador de la maestría genera oficio donde se especifica las materias que
-                                debe cursar
-                                y el costo de cada materia incluyendo el costo de inscripción (FPI-01A).</li>
-                            <li>El aspirante realiza el pago correspondiente en el Departamento de Recursos
-                                Financieros.</li>
-                            <li>El Departamento de Recursos Financieros elabora el recibo de pago.</li>
-                            <li>Presentarse en tiempo y forma con el coordinador de la maestría para realizar la
-                                inscripción
-                                con la constancia firmada por el Departamento de Servicios Escolares (FSI-01).</li>
-                            <li>El Coordinador inscribe al alumno e imprime horario.</li>
-                            <li>El alumno firma el horario.</li>
-                        </ol>
-                        <!--/div-->
-                        <br class="bloque">
-
-
-
 
                     </div>
 
