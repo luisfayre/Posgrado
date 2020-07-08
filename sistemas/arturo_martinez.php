d627d3cd95e08b1beea066d8e4746b5523c72736
SlickGeek <50652828+SlickGeek@users.noreply.github.com>
2020-07-08 14:03:50 -0600

    Create arturo_martinez.php

---
+++ sistemas/arturo_martinez.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap-->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <!-- Estilos-->
+        <link rel="stylesheet" href="../css/style_tec.css">
+        <link rel="stylesheet" href="../css/index.css">
+        <link rel="stylesheet" href="../css/style.css">
+        <!-- Funete-->
+        <link rel="stylesheet" href="../css/font-awesome.css">
+        <!-- Scripts-->
+        <script src="../js/jquery.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
+        <script src="../js/javascript.js"></script>
+        <script type="text/javascript" src="../js/jquery.slidepanel.js"></script>
+        <link rel="stylesheet" type="text/css" href="../css/jquery.slidepanel.css">
+
+        <title>Arturo Martínez Ayala</title>
+        <script src="../js/ddmenu.js" type="text/javascript"></script>
+    </head>
+
+    <body class="bodyIndex">
+        <a id="ddmenuHeader" href="headerDEPI.html"></a>
+        <!--Barra de navegacion-->
+        <a id="ddmenuLink" href="barraMSist.html"></a>
+        <div class="container" style="text-align:justify;">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2>DIVISIÓN DE ESTUDIOS DE
+                        POSGRADO E INVESTIGACIÓN
+                    </h2>
+                </div>
+            </div>
+            <div class="row bloque">
+                <h4 class="text-center">Docente </h4>
+                <div class="jumbotron cv">
+                    <div class="container">
+                        <div class="text-center">
+                            <img clas="img-responsive" src="../img/cv/desconocido.jpg" alt="">
+                            <h2>Dr. Arturo Martínez Ayala</h2>
+                            <p>CORREO: arturo.ma@chihuahua2.tecnm.mx
+                                <br> Instituto Tecnológico de Chihuahua II: (052) 614.4425000.124
+                            </p>
+                        </div>
+                        <p>ADSCRIPCIÓN ACTUAL
+                            <br> 15/12/2013 - 02/2020 </p>
+                        <blockquote>
+                            <p>TECNOLÓGICO NACIONAL DE MÉXICO / INSTITUTO TECNOLÓGICO DE CHIHUAHUA II, Profesor de tiempo completo</p>
+                        </blockquote>
+                        <p>ARTÍCULOS INDIZADOS</p>
+                        <blockquote>
+                            <table>
+                                <tr>
+                                    <td valign=top>2016&nbsp;</td>
+                                    <td><strong>EFFECT OF HEAT TREATMENT ON THE ELECTRICAL PROPERTIES OF PEROVSKITE SOLAR CELLS. SOLAR ENERGY MATERIALS AND SOLAR CELLS, </strong>Kumar, Y., Regalado-Pérez, E., Ayala, A. M., Mathews, N. R., & Mathew, X. 157, 10-17.
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2016&nbsp;</td>
+                                    <td><strong>SYNTHESIS OF PYRITE FES2 NANORODS BY SIMPLE HYDROTHERMAL METHOD AND ITS PHOTOCATALYTIC ACTIVITY, </strong>Morales-Gallardo, M. V., Ayala, A. M., Pal, M., Jacome, M. C., Antonio, J. T., & Mathews, N. R. Chemical Physics Letters, 660, 93-98.</td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2018&nbsp;</td>
+                                    <td><strong>IMPACT OF CD CONCENTRATIONS ON THE PHYSICAL PROPERTIES OF CU2 (CdxZn1-x) SNS4 THIN FILMS, </strong>Courel, M., Martinez-Ayala, A., Sanchez, T. G., Regalado-Perez, E., De Los Santos, I. M., Mathews, N. R., & Mathew, X. Superlattices and Microstructures, 122, 324-335.</td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2018&nbsp;</td>
+                                    <td><strong>Bi2S3 NANOPARTICLES BY FACILE CHEMICAL SYNTHESIS: ROLE OF PH ON GROWTH AND PHYSICAL PROPERTIES, </strong>Reynoso, Y. R., Martinez-Ayala, A., Pal, M., Paraguay-Delgado, F., & Mathews, N. R. Advanced Powder Technology, 29(12), 3561-3568.
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2019&nbsp;</td>
+                                    <td><strong>FACILE SYNTHESIS OF Cu2ZnGeS4 THIN FILMS FROM BINARY METAL SULFIDES AND STUDY OF THEIR PHYSICAL PROPERTIES., </strong>. Ayala, A. M., Mathews, N. R., Pal, M., Gupta, G. K., Dixit, A., & Mathew, X. Thin Solid Films, 676, 68-74.</td>
+                                </tr>
+                            </table>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Pie de pagina-->
+    <div class="container">
+        <?php
+        require_once './includes/footer.php';
+        ?>
+    </div>
+</body>
+
+</html>
