f3295ae9d807d04395cb0a3135299d47fb68eb14
SlickGeek <50652828+SlickGeek@users.noreply.github.com>
2020-07-09 22:57:00 -0600

    Update hernan_de_la_garza.php

--- sistemas/hernan_de_la_garza.php
+++ sistemas/hernan_de_la_garza.php
@@ -55,7 +55,40 @@
                             <table>
                                 <tr>
                                     <td valign=top>2012&nbsp;</td>
-                                    <td><strong>CLASIFICADOR VERSÁTIL</strong>, PROGRAMABLE USANDO IMÁGENES. ISBN: 978-1-4276-4803-7 . memorias del 5o. Congreso de Investigación y Posgrado del estado de Chihuahua .</td>
+                                    <td><strong>CLASIFICADOR VERSÁTIL PROGRAMABLE USANDO IMÁGENES</strong>, ISBN: 978-1-4276-4803-7. memorias del 5o. Congreso de Investigación y Posgrado del estado de Chihuahua.</td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2018&nbsp;</td>
+                                    <td><strong>R. OBEX - EXPLORADOR DE OBJETOS DE PROGRAMACIÓN EN UN AMBIENTE ERP</strong>, Torres, E; Alvarado G., J.A., De la Garza G., H; Sandoval, R. 1a Jornada de Ciencia y Tecnológica Aplicada, Tecnológico Nacional de México/CENIDET. ISSN en trámite. Cuernavaca, Morelos. </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2018&nbsp;</td>
+                                    <td><strong>CLASIFICACIÓN DE TEXTOS DE INFORMACIÓN PÚBLICA DEL ESTADO DE CHIHUAHUA CON TÉCNICAS DE INTELIGENCIA ARTIFICIAL</strong>, García, J; Alvarado Granadino, J. A.; De la Garza Gutiérrez, H; Legarda Sáenz, A. 1a Jornada de Ciencia y Tecnológica Aplicada, Tecnológico Nacional de México/CENIDET. ISSN en trámite. Cuernavaca, Morelos. </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2017&nbsp;</td>
+                                    <td><strong>INSPECCIÓN VISUAL AUTOMÁTICA EN TABLILLAS DE TELÉFONO CELULARES</strong>, Osio, A; De la Garza Gutiérrez, H; Legarda Sáenz, A.;  Alvarado Granadino, J. A. Revista Electrónica de Ingeniería y Aplicación. Instituto Tecnológico Superior de Huichapan, ISSN 2448-6817, año 3, núm. 3.</td>
+                                </tr>
+                            </table>
+                        </blockquote>
+                        <p>MEMORIAS EN CONGRESOS</p>
+                        <blockquote>
+                            <table>
+                                <tr>
+                                    <td valign=top>2019&nbsp;</td>
+                                    <td><strong>SOFTWARE DE RECONOCIMIENTO ÓPTICO DE CARACTERES</strong>, Romo Marín, D. F.; Alvarado Granadino, J. A.; Legarda Sáenz, A.; De la Garza Gutiérrez, H., Caldera Franco, M. I. Revista Digital. Memorias Congreso Internacional de Investigación Científica Multidisciplinaria. ISSN: 2395-9711. (1) 7. </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2018&nbsp;</td>
+                                    <td><strong>GHSOM EN LA DETECCIÓN DE LA POLARIDAD DE TEXTOS</strong>, Portillo T., E; Alvarado Granadino, J. A.; Legarda Sáenz, A.; De la Garza Gutiérrez, H. Revista Digital del Tecnológico de Monterrey, ISSN 2395-9711, Vol. 6 No. 1. </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2018&nbsp;</td>
+                                    <td><strong>APLICACIÓN MÓVIL PARA EL CONTROL Y GESTIÓN DE UNA RED DE DOMÓTICA</strong>, Alvarado, G., A.C.; Sandoval R., R.; Legarda Sáenz, A.; De la Garza Gutiérrez, H. Revista Digital del Tecnológico de Monterrey, ISSN 2395-9711, Vol. 6 No. 1.</td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2015&nbsp;</td>
+                                    <td><strong>SOFTWARE LIBRE DE CORRELACIÓN DIGITAL DE IMÁGENES EN 2D PARA DETERMINAR EL CAMPO DE DESPLAZAMIENTOS Y DEFORMACIONES EN PROBETAS PLANAS</strong>, Castañeda, R.; Días, A.; De la Garza Gutiérrez, H. XXX Congreso de Instrumentación SOMI, Durango, Dgo. </td>
                                 </tr>
                             </table>
                         </blockquote>
