bbf5f7322b4534fb31de0ea53d0fd6a2b80239d8
SlickGeek <50652828+SlickGeek@users.noreply.github.com>
2020-07-09 22:57:07 -0600

    Update marisela_caldera.php

--- sistemas/marisela_caldera.php
+++ sistemas/marisela_caldera.php
@@ -55,23 +55,106 @@
                             <table>
                                 <tr>
                                     <td valign=top>2012&nbsp;</td>
-                                    <td><strong>CLASIFICADOR VERSÁTIL</strong>, PROGRAMABLE DE PRODUCTOS USANDO IMÁGENES.5º Congreso Internacional de Investigación CIPITECH 2011.</td>
+                                    <td><strong>CLASIFICADOR VERSÁTIL PROGRAMABLE DE PRODUCTOS USANDO IMÁGENES</strong>. 5º Congreso Internacional de Investigación CIPITECH 2011.</td>
                                 </tr>
                                 <tr>
                                     <td valign=top>2013&nbsp;</td>
-                                    <td><strong>CLASIFICADOR VERSÁTIL</strong>, EVALUACIÓN POR COMPETENCIAS UTILIZANDO RÚBRICAS EN MOODLE. ISBN: 978-607-8262-01-4 . Cipitech.</td>
+                                    <td><strong>EVALUACIÓN POR COMPETENCIAS UTILIZANDO RÚBRICAS EN MOODLE</strong>. ISBN: 978-607-8262-01-4 . Cipitech.</td>
                                 </tr>
                                 <tr>
                                     <td valign=top>2014&nbsp;</td>
-                                    <td><strong>CLASIFICADOR VERSÁTIL</strong>, SOFTWARE ASÍNCRONO PARA LA GESTIÓN DEL CURSO EN EL SISTEMA DE GESTIÓN DE LA CALIDAD. 7MO. Congreso Internacional de Investigación CIPITECH 2014.</td>
+                                    <td><strong>SOFTWARE ASÍNCRONO PARA LA GESTIÓN DEL CURSO EN EL SISTEMA DE GESTIÓN DE LA CALIDAD</strong>. 7MO. Congreso Internacional de Investigación CIPITECH 2014.</td>
                                 </tr>
                                 <tr>
                                     <td valign=top>2014&nbsp;</td>
-                                    <td><strong>CLASIFICADOR VERSÁTIL</strong>, FACILITAR LA EVALUACIÓN MEDIANTE LA APLICACIÓN DE RÚBRICAS EN UNA PLATAFORMA DE APRENDIZAJE. Interno.</td>
+                                    <td><strong>FACILITAR LA EVALUACIÓN MEDIANTE LA APLICACIÓN DE RÚBRICAS EN UNA PLATAFORMA DE APRENDIZAJE</strong>. Interno.</td>
                                 </tr>
                                 <tr>
                                     <td valign=top>2015&nbsp;</td>
-                                    <td><strong>CLASIFICADOR VERSÁTIL</strong>, MODELO PARA EVALUAR LA RENTABILIDAD DE TESIS DOCTORALES: JUSTIFICACIÓN TEÓRICA Y APROXIMACIONES METODOLÓGICAS. REVISTA CIENCIAS DE LA DOCUMENTACIÓN ISSN 0719-5753 VOLUMEN 1 – NÚMERO 2.</td>
+                                    <td><strong>MODELO PARA EVALUAR LA RENTABILIDAD DE TESIS DOCTORALES: JUSTIFICACIÓN TEÓRICA Y APROXIMACIONES METODOLÓGICAS</strong>. Revista Ciencias de la Documentación ISSN 0719-5753 Volumen 1 – No. 2.</td>
+                                </tr>
+                            </table>
+                        </blockquote>
+                        <p>ARTÍCULOS INDEXADOS</p>
+                        <blockquote>
+                            <table>
+                                <tr>
+                                    <td valign=top>2019&nbsp;</td>
+                                    <td><strong>FIDELIDAD EN EL USO DE APP PARA LA RESOLUCIÓN DE ECUACIONES DIFERENCIALES</strong>. Camacho Ríos, A., Caldera Franco, M. I., & Valenzuela González, V. 11(1), 74-89. REVISTA: Apertura - Universidad de Guadalajara. ISSN: 2007-1094. </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2018&nbsp;</td>
+                                    <td><strong>APRENDER EL USO DEL TECLADO PARA NIÑOS CON DISCAPACIDAD VISUAL</strong>. Chavira-Celaya, L.; Caldera-Franco, M.I.; Nevárez-Chávez, L. REVISTA: RECIE. Revista Electrónica Científica de Investigación Educativa - Red de Investigadores Educativos Chihuahua AC ISSN: 2594-200X. </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2017&nbsp;</td>
+                                    <td><strong>MODELIZACIÓN DE UNA ACTIVIDAD DE LA FÍSICA PARA MEJORAR LA ENSEÑANZA DEL CONCEPTO DE FUNCIÓN</strong>. Camacho-Ríos, A.; Valenzuela-González, V.; Caldera-Franco, M.I.</td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2015&nbsp;</td>
+                                    <td><strong>MODELO PARA EVALUAR LA RENTABILIDAD DE TESIS DOCTORALES: JUSTIFICACIÓN TEÓRICA Y APROXIMACIONES METODOLÓGICAS</strong>. Caldera-Franco, M.; Tarango, J. Revista Ciencias de la Documentación. CHILE; ISSN: 0719-5753. </td>
+                                </tr>
+                            </table>
+                        </blockquote>
+                        <p>CAPÍTULO DE LIBRO</p>
+                        <blockquote>
+                            <table>
+                                <tr>
+                                    <td valign=top>2019&nbsp;</td>
+                                    <td><strong>ANÁLISIS COMPARATIVO DE APLICACIONES MÓVILES QUE GRAFICAN SERIES DE FOURIER.</strong> Duarte-Martínez, J.F.Camacho-Ríos, A.; Caldera-Franco, M. Proceedings TI. Ciencias de la Tierra, Física y Matemáticas. (Coord. Serrudo González, J.A.). ECORFAN. ISBN: 978-607-8534-90-6.</td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2019&nbsp;</td>
+                                    <td><strong>DESARROLLO DE UNA APLICACIÓN MÓVIL PARA RESOLVER ECUACIONES DIFERENCIALES ORDINARIAS (EDO). </strong>García-Almeida, D.I.; Camacho-Ríos, A.; Caldera-Franco, M.; Cuevas, H. Poceedings TI. Ciencias de la Tierra, Física y Matemáticas. (Coord. Serrudo González, J.A.). ECORFAN. ISBN: 978-6078534937.</td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2016&nbsp;</td>
+                                    <td><strong>MODELIZACIÓN ESCOLAR DE UNA PRÁCTICA DE INGENIERÍA. </strong>Sánchez-Luján, B.I.; Camacho-Ríos, A.; Caldera-Franco, M.I. LIBRO: La investigación en la educación superior y su vinculación en el estado, sociedad y economía - Cenid Editorial MÉXICO; ISBN: 978-607-8435-34-0.</td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2015&nbsp;</td>
+                                    <td><strong>ESTUDIO DE LA PERTINENCIA PARA LA MAESTRÍA EN SISTEMAS COMPUTACIONALES DEL ITCH II. </strong>Caldera-Franco, M.I.; Ronquillo-Maynez, G.; Nevarez-Chávez, L.; Ibarra-Murrieta, B.M. LIBRO: La Ciencia y Tecnológica al Servicio de la Sociedad. Editorial: Universidad Tecnológica de Cuidad Juárez. México; ISBN: 978-6078262076.</td>
+                                </tr>
+                            </table>
+                        </blockquote>
+                        <p>MEMORIAS EN CONGRESOS</p>
+                        <blockquote>
+                            <table>
+                                <tr>
+                                    <td valign=top>2019&nbsp;</td>
+                                    <td><strong>SOFTWARE DE RECONOCIMIENTO ÓPTICO DE CARACTERES. </strong>Romo Marín, D. F.; Alvarado Granadino, J. A.; Legarda Sáenz, A.; De la Garza Gutiérrez, H., Caldera Franco, M. I. Revista Digital. Memorias Congreso Internacional de Investigación Científica Multidisciplinaria. ISSN: 2395-9711. (1) 7. </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2018&nbsp;</td>
+                                    <td><strong>SISTEMA ADAPTATIVO DE APOYO EN COMPRENSIÓN LECTORA Y CÁLCULO MENTAL A NIVEL SECUNDARIA. </strong>Terrazas-Rodríguez, J.H.; Sánchez, L., I.L.; Caldera-Franco, M.I.; De la Garza-Gutiérrez, H. Revista Digital. Memorias Congreso Internacional de Investigación Científica Multidisciplinaria. ISSN: 2395-9711. (1) 7. </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2018&nbsp;</td>
+                                    <td><strong>APRENDIZAJE ADAPTATIVO MEDIANTE CONOCIMIENTOS PREVIOS PARA LA MATERIA DE FUNDAMENTOS DE PROGRAMACIÓN. </strong>Rivas-Hernández, G.H.; Nevárez-Chávez, L.; Caldera-Franco, M.I.; Ronquillo-Máynez, G.; De la Garza-Gutiérrez, H. Revista Digital. Memorias Congreso Internacional de Investigación Científica Multidisciplinaria. ISSN: 2395-9711. (1) 7. </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2018&nbsp;</td>
+                                    <td><strong>OBTENCIÓN RÁPIDA DE PREGUNTAS DE OPCIÓN MÚLTIPLE A PARTIR DE UNA LISTA DE ORACIONES TEXTUALES PARA LA GENERACIÓN DE EXÁMENES. </strong>Hernández-Carrasco, I.F.; Caldera-Franco, M.I.; Blanco-Vega, R.; Nevárez-Chávez, L.; Alvarado-Granadino, J.A. Revista Digital. Memorias Congreso Internacional de Investigación Científica Multidisciplinaria. ISSN: 2395-9711. (1) 7. </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2018&nbsp;</td>
+                                    <td><strong>ASISTENTE PARA EJERCICIOS DE REHABILITACIÓN PARA NIÑOS CON CAPACIDADES DIFERENTES. </strong>Quezada-Leyva, P.P.; Caldera-Franco, M.I.; Ronquillo-Máynez, G.; Nevárez-Chávez, L. Revista Digital. Memorias Congreso Internacional de Investigación Científica Multidisciplinaria. ISSN: 2395-9711. (1) 7. </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2017&nbsp;</td>
+                                    <td><strong>APLICACIÓN MÓVIL DE APOYO EN TERAPIA DE LENGUAJE PARA PACIENTES CON CAPACIDADES DIFERENTES. </strong>García-Seáñez, J.; Nevárez-Chávez, L.; Caldera-Franco, M.I.; Ronquillo-Máynez, G. Revista Digital. Memorias Congreso Internacional de Investigación Científica Multidisciplinaria. ISSN: 2395-9711. (1) 7. </td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2016&nbsp;</td>
+                                    <td><strong>EVALUACIÓN DE LA PERTINENCIA DE LAS TESIS DOCTORALES COMO OBJETOS DE APRENDIZAJE: DE LITERATURA GRIS A LA MEDICIÓN DEL IMPACTO. </strong>Caldera-Franco, M.I.; Tarango-Ortiz, J.; Ascencio-Baca, G. Revista Digital. Memorias Congreso Internacional de Investigación Científica Multidisciplinaria. ISSN: 2395-9711. (1) 7. /td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2016&nbsp;</td>
+                                    <td><strong>ENFOQUE ADAPTATIVO PARA LA CAPACITACIÓN DE USUARIOS EN EL SISTEMA DE CAJA DE AHORRO Y PRÉSTAMO. </strong>Nevárez-Chávez, L.; Ibarra-Murrieta, B.I.; Caldera-Franco, M.I. Tlamati Sabiduría. Memorias Congreso Internacional de Computación CICOM. ISSN: 2007-2066. (3) 7.</td>
+                                </tr>
+                                <tr>
+                                    <td valign=top>2015&nbsp;</td>
+                                    <td><strong>FACILITAR LA EVALUACIÓN MEDIANTE LA APLICACIÓN DE RÚBRICAS EN UNA PLATAFORMA DE APRENDIZAJE.</strong>Nevárez-Chávez, L.; Caldera-Franco, M.I. ANFEI Digital. ISSN: 2395-9878. (2) 1. </td>
                                 </tr>
                             </table>
                         </blockquote>
