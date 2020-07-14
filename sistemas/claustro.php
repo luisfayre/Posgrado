65e9b123e1869bce233f563f16ec781da0f4f6b6
SlickGeek <50652828+SlickGeek@users.noreply.github.com>
2020-07-14 13:12:15 -0600

    Update claustro.php

--- sistemas/claustro.php
+++ sistemas/claustro.php
@@ -76,10 +76,13 @@
                                             <td>01/07/2000 </td>
                                             <td><strong>Doctorado</strong> en Matemática Educativa. Centro de Investigación y Estudios Avanzados del Instituto Politécnico Nacional. </td>
                                         </tr>
-
                                         <tr>
                                             <td>01/07/1992 </td>
                                             <td><strong>Maestría</strong> en Matemática Educativa. Centro de Investigación y Estudios Avanzados del Instituto Politécnico Nacional. </td>
+                                        </tr>
+                                        <tr>
+                                            <td>01/02/1998</td>
+                                            <td><strong>Licenciatura</strong> en la enseñanza de las matemáticas. Universidad Autónoma de Ciudad Juárez. </td>
                                         </tr>
                                     </table>
                                     <p>
